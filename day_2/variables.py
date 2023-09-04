2e895f0abc5986e7c849babb80cbc1d2cf2f09ba
Rishabh Vaibhav <48403808+Rishabhvaibhav@users.noreply.github.com>
2023-09-05 00:42:36 +0530

    day9

---
+++ day_2/variables.py
@@ -0,0 +1,52 @@
+# Exercises: Level 1
+# Declare a first name variable and assign a value to it
+first_name = "Rishabh"
+print(first_name)
+# Declare a last name variable and assign a value to it
+last_name = "Vaibhav"
+print(last_name)
+# Declare a full name variable and assign a value to it
+full_name = first_name + last_name
+print(full_name)
+# Declare a country variable and assign a value to it
+country = 'India'
+print(country)
+# Declare a city variable and assign a value to it
+city = "bangalore"
+# Declare an age variable and assign a value to it
+age = 21
+print(age)
+# print(age)
+# Declare a year variable and assign a value to it
+year = 2023
+# Declare a variable is_married and assign a value to it
+is_married = False
+# Declare a variable is_true and assign a value to it
+is_true = False
+# Declare a variable is_light_on and assign a value to it
+is_light_on = True
+# Declare multiple variable on one line
+
+multiple , variable = "multiple" , "Variable"
+
+# Exercises: Level 2
+# Check the data type of all your variables using type() built-in function
+print(type(full_name))
+# Using the len() built-in function, find the length of your first name
+print(len(first_name))
+# Compare the length of your first name and your last name
+
+# Declare 5 as num_one and 4 as num_two
+# Add num_one and num_two and assign the value to a variable total
+# Subtract num_two from num_one and assign the value to a variable diff
+# Multiply num_two and num_one and assign the value to a variable product
+# Divide num_one by num_two and assign the value to a variable division
+# Use modulus division to find num_two divided by num_one and assign the value to a variable remainder
+# Calculate num_one to the power of num_two and assign the value to a variable exp
+# Find floor division of num_one by num_two and assign the value to a variable floor_division
+# The radius of a circle is 30 meters.
+# Calculate the area of a circle and assign the value to a variable name of area_of_circle
+# Calculate the circumference of a circle and assign the value to a variable name of circum_of_circle
+# Take radius as user input and calculate the area.
+# Use the built-in input function to get first name, last name, country and age from a user and store the value to their corresponding variable names
+# Run help('keywords') in Python shell or in your file to check for the Python reserved words or keywords
