3d3b87bd340d2985ea094c00b2cfef06fabc3772
Rishabh Vaibhav <48403808+Rishabhvaibhav@users.noreply.github.com>
2023-09-10 15:09:43 +0530

    practice

---
+++ Practice_Question/practices.py
@@ -0,0 +1,9 @@
+# 8. Find the square root of a number.
+def findsqroot(num):
+    num = int(input("Enter the number "))
+    squareroot = num * num
+    print(squareroot)
+    return num
+
+findsqroot(num)
+    
