b316a1f2aa3f406d9a12a69df83021a401f997ca
Rishabh Vaibhav <48403808+Rishabhvaibhav@users.noreply.github.com>
2023-09-13 19:04:20 +0530

    day 11

--- Practice_Question/practices.py
+++ Practice_Question/practices.py
@@ -1,9 +1,34 @@
-# 8. Find the square root of a number.
-def findsqroot(num):
-    num = int(input("Enter the number "))
-    squareroot = num * num
-    print(squareroot)
-    return num
+# pizza order program 
 
-findsqroot(num)
-    
+size = input("Enter the size you want \n Small = s \n Medium = m \n Large= l \n Pick Any pizza You want :  ")
+
+if size == "s" or"S":
+    cost = 100
+    print("you choose small pizza ")
+elif size == "m" or "M":
+    cost = 200
+    print("you choose medium pizza ")
+elif size == "l" or "L":
+    cost = 300
+    print("you choose large pizza ")
+else:
+    exit()
+
+addon = input("if want to add peperroni enter Y / N ")
+
+if addon == "y" or "Y":
+    if cost == 100:
+         cost = cost + 30
+         print(f"Your current cost is {cost}")
+    elif cost == 200:
+         cost = cost + 50
+         print(f"Your current cost is {cost}")
+    elif cost == 300:
+         cost = cost + 80
+         print(f"Your current cost is {cost}")
+    else: 
+        pass
+cheese = input("you want extra cheese :Y / N")   
+if cheese == "y" or "Y":
+    cost = cost + 20
+    print(f"current code is : {cost}")
---
+++ Practice_Question/tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+size = input("Enter the size you want \n Small = s \n Medium = m \n Large= l \n Pick Any pizza You want :  ")
+
+# if size == "s" or"S":
+#     cost = 100
+#     print("you choose small pizza ")
+#     if size == "m" or "M":
+#         cost = 200
+#         print("you choose medium pizza ")
+#         if size == "l" or "L":
+#             cost = 300
+#             print("you choose large pizza ")
+# else:
+#     None
