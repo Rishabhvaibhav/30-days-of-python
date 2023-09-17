33dd8f1b0507a4e1da005d4a350b3a518ebca975
Rishabh Vaibhav <48403808+Rishabhvaibhav@users.noreply.github.com>
2023-09-18 03:31:00 +0530

    Update scap.py

--- scap.py
+++ scap.py
@@ -1,20 +1,10 @@
-# create a rock paper scissor 
-import random
-name = input("Enter the Name : ")
-while True:
-        for i in range():
-        user_input = int(input("Choose what your choose \n 0 = Rock \n 1 = paper \n 2 = Scissor  \n Select One :    "))
-        print(user_input)
-        computer_choose = random.randint(0,2)
+# find square of list 
 
-        if user_input == computer_choose:
-            print("Draw")
-        elif (user_input == 0) and ( computer_choose == 1):
-            print(f"-----------------{name} win-------------------\n")
-        elif (user_input == 2) and ( computer_choose == 1):
-            print(f"----------------{name} win--------------------\n")
-        elif (user_input == 0) < ( computer_choose == 2):
-            print(f"----------------{name} win---------------------\n")
-        else:
-            print(f"computer choose {computer_choose}")
-            print("------------------Computer wins-----------------\n")
+
+numbers = [1,5,4,3,78,56,89,45,12,3]
+sq = []
+for i in numbers:
+    square = i ** 2
+    sq.append(square)
+    
+print("the list of square is : " , sq)
