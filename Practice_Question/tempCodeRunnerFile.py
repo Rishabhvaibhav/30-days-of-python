b56f97915e56d8091991bb5c307bbc207edbda90
Rishabh Vaibhav <48403808+Rishabhvaibhav@users.noreply.github.com>
2023-09-21 13:51:06 +0530

    update

--- Practice_Question/tempCodeRunnerFile.py
+++ Practice_Question/tempCodeRunnerFile.py
@@ -1,13 +1,2 @@
-size = input("Enter the size you want \n Small = s \n Medium = m \n Large= l \n Pick Any pizza You want :  ")
-
-# if size == "s" or"S":
-#     cost = 100
-#     print("you choose small pizza ")
-#     if size == "m" or "M":
-#         cost = 200
-#         print("you choose medium pizza ")
-#         if size == "l" or "L":
-#             cost = 300
-#             print("you choose large pizza ")
-# else:
-#     None
+# convert binary to decimal then check that is divisiable or not 
+# check the number is prime or not 
