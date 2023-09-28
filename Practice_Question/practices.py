87167d731e1c57016d6735b955d456079ef9edcb
Rishabh Vaibhav <48403808+Rishabhvaibhav@users.noreply.github.com>
2023-09-28 16:24:56 +0530

    update

--- Practice_Question/practices.py
+++ Practice_Question/practices.py
@@ -1,21 +1,11 @@
-user_input = input("Enter the number you want : ")
-user_input = user_input.split(" ")
-# 78 56 45 7 88 9 
+#  0 1 1 2 3 5 8 12
 
-count = 0
-for i in user_input:
-    count += 1
-print(count)    
+user_input= int(input( ))
 
-for int_value in range(count):
-    user_input[int_value] = int(user_input[int_value])
+first_no = 0
+second_no = 1
+fibo = [0]
+
+for i in range(user_input):
+    user_input[0] 
     
-max_num = user_input[0]
-
-for number in user_input:
-    if number < int(max_num):
-        max_num = number
-        
-print(number)
-    
-
