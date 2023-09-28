18abe5ef6185056351805437bac3b16dfc006c6b
Rishabh Vaibhav <48403808+Rishabhvaibhav@users.noreply.github.com>
2023-09-28 15:38:41 +0530

    update

--- Practice_Question/practices.py
+++ Practice_Question/practices.py
@@ -1,18 +1,21 @@
-# Calculate the value of y (y = x^2 + 6x + 9). 
-# Try to use different x values and figure out at what x value y is going to be 0.
-# for i in range(1)
+user_input = input("Enter the number you want : ")
+user_input = user_input.split(" ")
+# 78 56 45 7 88 9 
 
+count = 0
+for i in user_input:
+    count += 1
+print(count)    
 
-# y = (x^2) + (6*x + 9)
-# print(y)
-"""
-list of skills : python , SQL , Django , pandas , numpy , pyspark , html , css, javascript 
+for int_value in range(count):
+    user_input[int_value] = int(user_input[int_value])
+    
+max_num = user_input[0]
 
-i want to create a timesheet for learning all the skills and modules 
-with 2 month of time duration , 
-create the excel sheet format , like a daily tracker 
-to fastest way to complete whole topic in 2 month only 
-genererate skils and topic day wise 
-with familier whole basic to advanced
-i allready clear some basic of python ,
-generate according to that 
+for number in user_input:
+    if number < int(max_num):
+        max_num = number
+        
+print(number)
+    
+
